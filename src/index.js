5b45d70aecfa2ef26c6e932ac076d1568b214c3a
SanjeevPrakash04 <jonathan@proliteautomation.com>
2025-02-11 17:01:53 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
-root.render(<App />);
+root.render(
+    <HashRouter>
+        <App />
+    </HashRouter>
+);
